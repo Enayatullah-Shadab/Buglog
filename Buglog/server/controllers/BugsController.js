9c06549ded874385b6550339f7ac691bc138ac21
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-15 10:37:36 -0600

    Bug Service and Controller completed

--- Buglog/server/controllers/BugsController.js
+++ Buglog/server/controllers/BugsController.js
@@ -7,16 +7,56 @@ export class BugsController extends BaseController {
   constructor() {
     super('api/bugs')
     this.router
-      // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('', this.getAll)
+      .get('/:id', this.getById)
       .post('', this.create)
+      .put('/:id', this.update)
+      .put('/:id', this.destroy)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const bug = await bugsService.getAll()
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const bug = await bugsService.getById(req.params.id)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async create(req, res, next) {
     try {
-      // DO NOT TRUST THE CLIENT
       req.body.creatorId = req.userInfo.id
       const bug = await bugsService.create(req.body)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const bug = await bugsService.update()
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async destroy(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const bug = await bugsService.destroy()
       res.send(bug)
     } catch (error) {
       next(error)
--- Buglog/server/services/BugsService.js
+++ Buglog/server/services/BugsService.js
@@ -1,9 +1,25 @@
 import { dbContext } from '../db/DbContext'
 
 class BugsService {
+  async getAll(query = {}) {
+    return await dbContext.Bugs.find(query).populate('creator')
+  }
+
+  async getById(id) {
+    return await dbContext.Bugs.findById(id).populate('creator')
+  }
+
   async create(body) {
     const bug = await dbContext.Bugs.create(body)
     return await dbContext.Bugs.findById(bug._id).populate('creator', 'name picture')
+  }
+
+  async update(body) {
+    return await dbContext.Bugs.findByIdAndUpdate(body.id).populate('creator')
+  }
+
+  async destroy(id) {
+    return await dbContext.Bugs.findByIdAndDelete(id)
   }
 }
 
